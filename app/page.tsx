80f82500246a667b56afb54ada8f74b0f305dbb3
alfred <alfredofernandezcarlos@gmail.com>
2023-12-14 15:12:09 -0400

    add filter by score

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,30 @@
 import { Data } from "@/typescript/comment"
 import { CommentsList } from "./_components/comments-list"
 import { rq } from '@/app/libs/axios';
+import ErrorComponent from "@/components/error-component";
 
 
-const fetchData = async ():Promise<Data | undefined> => {
+const fetchData = async ():Promise<Data> => {
   try {
     const res = await rq.get('api/comments')
     return res.data
   } catch (error){
-    console.log(error, 'Error trying to fetch comments')
-    return undefined
+    console.log(error, 'Error trying to fetch comments', error)
+    throw error
   }
 }
 
 export default  async function Home() {
-  const data =  await fetchData()
+  try {
+    const data =  await fetchData()
+    const filteredComments = data.comments.sort((a,b) => b.score - a.score)
 
-  if (!data) return <div>Something went wrong</div>
-
-  return (
-    <article className="bg-neutral-very-light-gray">
-      <CommentsList comments={data.comments} user={data.currentUser} />
-    </article>
-  )
+    return (
+      <article className="bg-neutral-very-light-gray">
+        <CommentsList comments={filteredComments} user={data.currentUser} />
+      </article>
+    )
+  } catch (error:any) {
+    return <ErrorComponent message={error?.response?.data || 'Something went wrong'}></ErrorComponent>
+  }
 }
