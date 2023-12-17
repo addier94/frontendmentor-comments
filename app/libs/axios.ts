e1b55a6658ad0adb6e1f0c3856894758e6d182bd
alfred <alfredofernandezcarlos@gmail.com>
2023-12-16 20:54:29 -0400

    changed baseURL

--- app/libs/axios.ts
+++ app/libs/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const rq = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://frontendmentor-comments-ahkd6wts1-addier94.vercel.app',
 });
