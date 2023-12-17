f5478217cfc2eabe9d28f525e3c8410a2d6aa0fd
alfred <alfredofernandezcarlos@gmail.com>
2023-12-16 21:06:47 -0400

    changed to localhost again

--- app/libs/axios.ts
+++ app/libs/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const rq = axios.create({
-  // baseURL: 'http://localhost:3000',
-  baseURL: 'https://frontendmentor-comments-ahkd6wts1-addier94.vercel.app',
+  baseURL: 'http://localhost:3000',
 });
